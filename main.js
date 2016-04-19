d55830a66d359a824af2a5a84fc7eb36deb9c0ab
Ryan Jacobs <jacobs9590@gmail.com>
2016-04-19 00:04:50 -0400

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+var read = require("read")
+
+var User = require("./lib/user.js")
+var Quiz = require("./lib/quiz.js")
+var Question = require("./lib/question.js")
+
+user1 = new User("jop")
+user1.start()
